82d7fd1464561654ad3faa5308601af6d2fd4d55
Prasang41 <harshitchaudhary568@gmail.com>
2025-10-16 11:26:52 +0530

    Theme Switcher

--- Theme Switcher/src/App.tsx
+++ Theme Switcher/src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { atom, useAtom } from 'jotai'
 
 const themer = atom('light');
 
 function App() {
-  const [count, setCount] = useState(0)
   const [theme, setTheme] = useAtom(themer);
 
   const onHandleTheme = () => {
@@ -15,33 +10,12 @@ function App() {
   }
 
   return (
-    <>
-      <div className='bg-white'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <div>
+    <div className={theme === "light" ? "bg-black text-white h-[100vh]" : "bg-white text-black h-[100vh]"}>
+      <div className="flex flex-col justify-center items-center h-[100vh]" data-theme={theme}>
         <h1>This is a theme switcher</h1>
-        <button onClick={onHandleTheme}>{theme === 'light' ? "Dark" : "Light"}</button>
+        <button className='border p-2 cursor-pointer' onClick={onHandleTheme}>{theme === 'light' ? "Dark" : "Light"}</button>
       </div>
-    </>
+    </div>
   )
 }
 
